4e7a9f609dac4fc68b5e060187444f03c78a5f61
seif <seif@gmail.com>
2026-03-01 00:16:39 +0200

    Separate logic from AuthController

--- DataAccess/Repositories/Repos/AuthRepository.cs
+++ DataAccess/Repositories/Repos/AuthRepository.cs
@@ -11,13 +11,52 @@ namespace ELProject.DataAccess.Repositories.Repos
 {
     public class AuthRepository
     {
-        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly UserManager<ApplicationUser> userManager;
         private readonly IConfiguration _config;
+        private readonly IFileStorageService fileStorageService;
 
-        public AuthRepository(UserManager<ApplicationUser> userManager, IConfiguration config)
+        public AuthRepository(UserManager<ApplicationUser> _userManager,
+            IConfiguration config,
+            IFileStorageService _fileStorageService)
         {
-            _userManager = userManager;
+            userManager = _userManager;
             _config = config;
+            fileStorageService = _fileStorageService;
+        }
+
+        public async Task<IdentityResult> RegisterAsync(RegisterDto UserDto)
+        {
+
+            ApplicationUser user = new();
+            user.UserName = UserDto.Username;
+            user.Gender = UserDto.Gender;
+
+            if (UserDto.ProfileImageFile != null)
+                ///<summary>
+                /// The Storing of ProfileImage now is completely generic.
+                /// - Maybe today you save ProfileImage in wwwroot
+                /// - Tomorrow in Azure Blob
+                /// - After that in AWS S3
+                ///</summary>
+                user.ProfileImage = await fileStorageService.SaveFileAsync(UserDto.ProfileImageFile);
+
+            // Assign role to user
+            await userManager.AddToRoleAsync(user, UserDto.Role.ToString());
+
+            // Save in DB
+            IdentityResult result = await userManager.CreateAsync(user, UserDto.Password);
+            return result;
+        }
+
+        public async Task<ApplicationUser> LoginAsync(LoginDto dto)
+        {
+            // Check of account is exists
+            ApplicationUser userFromDb = await userManager.FindByNameAsync(dto.Username);
+
+            if (userFromDb != null)
+                await userManager.CheckPasswordAsync(userFromDb, dto.Password);
+
+            return userFromDb;
         }
 
         public async Task<string> GetTokenAsync(ApplicationUser user)
@@ -28,7 +67,7 @@ namespace ELProject.DataAccess.Repositories.Repos
                 new Claim(ClaimTypes.Name, user.UserName!)
             };
 
-            var UserRoles = await _userManager.GetRolesAsync(user);
+            var UserRoles = await userManager.GetRolesAsync(user);
             foreach (var role in UserRoles)
                 claims.Add(new Claim(ClaimTypes.Role, role));
 
