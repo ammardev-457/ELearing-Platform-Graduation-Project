a05eeca7e3cb7fea31c05561a6bf28a3e8758b5b
seif <seif@gmail.com>
2026-05-14 15:21:41 +0300

    Add Explanation Column

---
+++ Migrations/20260514121742_AddExplainationColumn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ELProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddExplainationColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Explanation",
+                table: "Questions",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Explanation",
+                table: "Questions");
+        }
+    }
+}
